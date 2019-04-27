b4ad93c80ef387c2e0cde22c052af236cf5c6e33
rudovik <giguni@ukr.net>
2019-04-27 20:53:25 +0300

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log('Client side javascript file is loaded.');
-
-// fetch('http://localhost:3000/weather?address=Ovruch').then((response) => {
-//   response.json().then((data) => {
-//     if (data.error) {
-//       console.log('Error: ' + data.error);
-//     } else {
-//       console.log(data.forecast);
-//       console.log(data.location);
-//     }
-//   });
-// });
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
